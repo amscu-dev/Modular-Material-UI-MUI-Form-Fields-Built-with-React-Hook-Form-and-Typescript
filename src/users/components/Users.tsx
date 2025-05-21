629c1cb2b8628eeebc167e9b93ab6322a82b94ea
amscu-dev <amarinescu.dev@gmail.com>
2025-05-21 23:14:20 +0300

    feat(zod-validation) refine zod valdiation for dynamic fields

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,8 +1,8 @@
-import { Stack, TextField } from "@mui/material";
-import { useFormContext } from "react-hook-form";
+import { Button, Container, Stack, TextField } from "@mui/material";
+import { useFieldArray, useFormContext, useWatch } from "react-hook-form";
 import type { Schema } from "../types/schema";
 import RHFAutocomplete from "../../components/RHFAutocomplete";
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import {
   useGenders,
   useLanguages,
@@ -12,6 +12,10 @@ import {
 import RHFToggleButtonGroup from "../../components/RHFToggleButtonGroup";
 import { RHFRadioGroup } from "../../components/RHFRadioGroup";
 import { RHFCheckbox } from "../../components/RHFCheckbox";
+import { RHFDateTimePicker } from "../../components/RHFDateTimePicker";
+import { RHFSlider } from "../../components/RHFSlider";
+import { RHFSwitch } from "../../components/RHFSwitch";
+import { RHFTextField } from "../../components/RHFTextField";
 
 function Users() {
   const statesQuery = useStates();
@@ -19,11 +23,7 @@ function Users() {
   const gendersQuery = useGenders();
   const skillsQuery = useSkills();
 
-  const {
-    register,
-    formState: { errors },
-    watch,
-  } = useFormContext<Schema>();
+  const { watch, control, unregister } = useFormContext<Schema>();
 
   // Asta sau DevTool pentru a urmari datele din formular in timp real.
   useEffect(() => {
@@ -33,40 +33,73 @@ function Users() {
     return () => sub.unsubscribe();
   }, [watch]);
 
+  const isTeacher = useWatch({ control, name: "isTeacher" });
+
+  const { append, fields, remove, replace } = useFieldArray({
+    control,
+    name: "students",
+  });
+
+  useEffect(() => {
+    if (!isTeacher) {
+      replace([]);
+      unregister("students");
+    }
+  }, [isTeacher, replace, unregister]);
+
   return (
-    <Stack sx={{ gap: 2 }}>
-      <TextField
-        {...register("name")}
-        label="Name"
-        error={!!errors.name}
-        helperText={errors.name?.message}
-      />
-      <TextField
-        {...register("email")}
-        label="Email"
-        error={!!errors.email}
-        helperText={errors.email?.message}
-      />
-      <RHFAutocomplete<Schema>
-        name="states"
-        label="States"
-        options={statesQuery.data}
-      />
-      <RHFToggleButtonGroup<Schema>
-        name="languagesSpoken"
-        options={languagesQuery.data}
-      />
-      <RHFRadioGroup<Schema>
-        name="gender"
-        options={gendersQuery.data}
-        label="Gender"
-      />
-      <RHFCheckbox<Schema>
-        name="skills"
-        options={skillsQuery.data}
-        label="Skills"
-      />
-    </Stack>
+    <Container maxWidth="sm" component="form">
+      <Stack sx={{ gap: 2 }}>
+        <RHFTextField<Schema> name="name" label="Name" />
+        <RHFTextField<Schema> name="email" label="Email" />
+        <RHFAutocomplete<Schema>
+          name="states"
+          label="States"
+          options={statesQuery.data}
+        />
+        <RHFToggleButtonGroup<Schema>
+          name="languagesSpoken"
+          options={languagesQuery.data}
+        />
+        <RHFRadioGroup<Schema>
+          name="gender"
+          options={gendersQuery.data}
+          label="Gender"
+        />
+        <RHFCheckbox<Schema>
+          name="skills"
+          options={skillsQuery.data}
+          label="Skills"
+        />
+        <RHFDateTimePicker<Schema>
+          name="registrationDateAndTime"
+          label="Registration Date & Time"
+        />
+        <RHFSlider<Schema> name="salaryRange" label="Salary Range" />
+        <RHFSwitch<Schema> name="isTeacher" label="Are you a teacher ?" />
+        {isTeacher && (
+          <Button onClick={() => append({ name: "" })} type="button">
+            Add new student
+          </Button>
+        )}
+        {fields.map((field, index) => (
+          <Fragment>
+            <RHFTextField
+              name={`students.${index}.name`}
+              label="Name"
+              key={field.id}
+            />
+            <Button onClick={() => remove(index)} type="button" color="error">
+              Remove
+            </Button>
+          </Fragment>
+        ))}
+      </Stack>
+      <Stack sx={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <Button type="submit">New User</Button>
+        <Button type="reset">Reset</Button>
+      </Stack>
+    </Container>
   );
 }
 
--- src/users/types/schema.ts
+++ src/users/types/schema.ts
@@ -1,28 +1,60 @@
 import { z } from "zod";
 import { patterns } from "../../constants";
 
-export const schema = z.object({
-  name: z.string().min(1, { message: "This field is required!" }),
-  email: z
-    .string()
-    .min(1, { message: "This field is required" })
-    .refine((emailValue) => patterns.email.test(emailValue), {
-      message: "Email is not valid!",
-    }), // testam cu regex ul si returnam true sau false
+// type Type1 = {};
+// type Type2 = {};
+// type Type3 = {};
+// type Type4 = Type1 & (Type2 | Type3); -- discriminated union types
 
-  states: z.array(z.string()).min(1).max(2),
-  languagesSpoken: z.array(z.string()),
-  gender: z.string().min(1),
-  skills: z.array(z.string()).min(1),
-});
+export const schema = z
+  .intersection(
+    z.object({
+      name: z.string().min(1, { message: "This field is required!" }),
+      email: z
+        .string()
+        .min(1, { message: "This field is required" })
+        .refine((emailValue) => patterns.email.test(emailValue), {
+          message: "Email is not valid!",
+        }), // testam cu regex ul si returnam true sau false
+
+      states: z.array(z.string()).min(1).max(2),
+      languagesSpoken: z.array(z.string()),
+      gender: z.string().min(1),
+      skills: z.array(z.string()).max(2),
+      registrationDateAndTime: z.date(),
+      salaryRange: z.array(z.number()).min(2).max(2),
+    }),
+    z.discriminatedUnion("variant", [
+      z.object({ variant: z.literal("create") }),
+      z.object({ variant: z.literal("update"), id: z.string().min(1) }),
+    ])
+  )
+  .and(
+    z.union([
+      z.object({ isTeacher: z.literal(false) }),
+      z.object({
+        isTeacher: z.literal(true),
+
+        students: z.array(
+          z.object({
+            name: z.string().min(4),
+          })
+        ),
+      }),
+    ])
+  );
 
 export type Schema = z.infer<typeof schema>;
 
 export const defaultValues: Schema = {
+  variant: "create",
   email: "",
   name: "",
   states: [],
   languagesSpoken: [],
   gender: "",
   skills: [],
+  registrationDateAndTime: new Date(),
+  salaryRange: [0, 2000],
+  isTeacher: false,
 };
